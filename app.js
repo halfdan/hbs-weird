97298b1298710f0db38b8e965f4e506cd5514c76
Fabian Becker <halfdan@xnorfz.de>
2014-02-08 01:28:22 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require('express'),
+    hbs     = require('express-hbs'),
+    when    = require('when'),
+    // Init app
+    app     = express();
+
+// Use `.hbs` for extensions and find partials in `views/partials`.
+app.engine('hbs', hbs.express3({
+  partialsDir: __dirname + '/views/partials'
+}));
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
+app.get('/', function(req, res){
+  res.render('index', {
+    title: 'Some title',
+    name: 'Weird express-hbs behaviour'
+  });
+});
+
+function weird() {
+  return when(function() { return 12; }, function(val){return val;});  
+}
+
+// Register an async handlebars helper for a given handlebars instance
+function registerAsyncHelper(hbs, name, fn) {
+  hbs.registerAsyncHelper(name, function (options, cb) {
+    // Wrap the function passed in with a when.resolve so it can
+    // return either a promise or a value
+    when.resolve(fn.call(this, options)).then(function (result) {
+      cb(result);
+    }); // ignore failure
+  });
+}
+
+registerAsyncHelper(hbs, 'weird', weird);
+
+app.listen(3000);
+
